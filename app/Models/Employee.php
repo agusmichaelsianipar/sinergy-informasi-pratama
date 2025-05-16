d74ce9b763ed1e77f8d024a04d46038099518c8c
Agus Michael Sianipar <agusmichaelsianipar99@gmail.com>
2025-05-16 18:05:38 +0700

    table model

---
+++ app/Models/Employee.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Employee extends Model
+{
+    use HasFactory, SoftDeletes;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'employee_id_number',       // YYYYMMDDYYYYMMGXXXXX
+        'citizenship_id_no',
+        'citizenship_id_file',
+        'date_of_birth',
+        'gender',
+        'phone',
+        'position',
+        'street',
+        'city_id',
+        'city_name',
+        'province_id',
+        'province_name',
+        'zip_code',
+        'bank_account',
+        'account_number',
+        $table->softDeletesTz('deleted_at')->nullable(),
+    ];
+}
