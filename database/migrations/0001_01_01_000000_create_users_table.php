176f6f4b7b9840f4e3843106e15bac407e06fb6b
Agus Michael Sianipar <agusmichaelsianipar99@gmail.com>
2025-05-16 18:05:29 +0700

    table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('user_status')->default(false);
+            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
-            $table->timestamps();
+            $table->softDeletesTz('deleted_at')->nullable();
+            $table->timestampsTz();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
---
+++ database/migrations/2025_05_16_094345_create_employees_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('employee_id_number', 20);
+            $table->string('citizenship_id_no', 16);
+            $table->text('citizenship_id_file');
+            $table->date('date_of_birth');
+            $table->char('gender', 1);
+            $table->string('phone', 16);
+            $table->string('position', 50);
+            $table->string('street', 250);
+            $table->bigInteger('city_id');
+            $table->string('city_name', 250);
+            $table->bigInteger('province_id');
+            $table->string('province_name', 250);
+            $table->string('zip_code', 10);
+            $table->string('bank_account', 100);
+            $table->string('account_number', 20);
+            $table->softDeletesTz('deleted_at')->nullable();
+            $table->timestampsTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
