2e54605496c799ce6eea056931db061592abc40e
Agus Michael Sianipar <agusmichaelsianipar99@gmail.com>
2025-05-17 15:57:12 +0700

    service provicer to binding repos and interface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\Repository\UserRepository;
+use App\Repository\EmployeeRepository;
 use Illuminate\Support\ServiceProvider;
+use App\Interfaces\UserRepositoryInterface;
+use App\Interfaces\EmployeeRepositoryInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(EmployeeRepositoryInterface::class, EmployeeRepository::class);
     }
 
     /**
