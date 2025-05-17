c5a277248ac2755c1c6e3ad05b74c09508a81377
Agus Michael Sianipar <agusmichaelsianipar99@gmail.com>
2025-05-17 16:01:20 +0700

    adding transaction and model not found exception

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -4,17 +4,27 @@ namespace App\Http\Controllers;
 
 use App\Models\Employee;
 use Illuminate\Http\Request;
+use App\Services\EmployeeService;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests\Employee\StoreEmployeeRequest;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class EmployeeController extends Controller
 {
+    protected $employeeService;
+    public function __construct(EmployeeService $employeeService){
+
+        $this->employeeService = $employeeService;
+    }
     /**
      * Display a listing of the resource.
      */
     public function index(Request $request)
     {
         $employees = Employee::all();
-        
-        return view('employee.index', compact('employees'));
+        $positions = Employee::POSITION;
+        $banks = Employee::BANK_ACCOUNT;
+        return view('employee.index', compact('employees', 'positions', 'banks'));
     }
 
     /**
@@ -28,9 +38,31 @@ class EmployeeController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreEmployeeRequest $request)
     {
-        //
+        try{
+            DB::transaction(function() use($request){
+                
+                $this->employeeService->storeEmployee($request);
+            });
+
+            return response()->json([
+                "status" => true,
+                "message" => "New employee successfully stored."
+            ], 200);
+        } catch (ModelNotFoundException $ex) {
+            
+            return response()->json([
+                'status' => false,
+                'error' => $ex->getModel()." not found!"
+            ], 404);
+        }catch(\Throwable $th){
+            
+            return response()->json([
+                'status' => false,
+                'error' => $th->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -52,9 +84,25 @@ class EmployeeController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $employeeIDNo)
     {
-        //
+        try{
+            DB::transaction(function() use($request, $employeeIDNo){
+                
+                $this->employeeService->updateEmployee($request, $employeeIDNo);
+            });
+
+            return response()->json([
+                "status" => true,
+                "message" => "Employee data successfully updated."
+            ], 200);
+        }catch(\Throwable $th){
+            
+            return response()->json([
+                'status' => false,
+                'error' => $th->getMessage()
+            ], 500);
+        }
     }
 
     /**
